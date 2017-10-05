d39296eb4e5bf8251a10a3f12c3816b6b80e9e77
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-05 09:15:10 +0200

    El programa solo muestra la frase "Hola Mundo"

---
+++ src/holamundocod/HolaMundoCoD.java
@@ -0,0 +1,14 @@
+
+package holamundocod;
+
+/**
+ * @author jborrajorodriguez
+ */
+public class HolaMundoCoD {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
