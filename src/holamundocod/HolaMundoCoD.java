cda48de6f7133a5973fa5ebd10dc30bb24253f64
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-05 09:22:24 +0200

    Se añadieron nuevas lineas para comprobar el funcionamiento del control de versiones.

--- src/holamundocod/HolaMundoCoD.java
+++ src/holamundocod/HolaMundoCoD.java
@@ -9,6 +9,8 @@ public class HolaMundoCoD {
    
     public static void main(String[] args) {
         System.out.println("Hola DAM01");//Muestra en pantalla "Hola mundo"
+        System.out.println("Hola Mundo");
+        //Prueba para comprobar que al poner una linea nueva se muestra en color verde
     }
    
 }
