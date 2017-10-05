85f911bc35a686fa0c7f4321d09c262adcfd76ca
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-05 09:36:15 +0200

    Borrado de lineas en blanco

--- src/holamundocod/HolaMundoCoD.java
+++ src/holamundocod/HolaMundoCoD.java
@@ -1,12 +1,8 @@
-
 package holamundocod;
-
 /**
  * @author jborrajorodriguez
  */
 public class HolaMundoCoD {
-
-   
     public static void main(String[] args) {
         System.out.println("Hola DAM01");//Muestra en pantalla "Hola mundo"
         System.out.println("Hola Mundo");
